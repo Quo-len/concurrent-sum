106288b9e01c4b8c966ef40baf319fc1708e1659
Vadym <v.kotenko003@gmail.com>
2025-12-10 17:41:40 +0200

    fix: output logs

--- mpi-sum-c/partial_sum.c
+++ mpi-sum-c/partial_sum.c
@@ -10,54 +10,39 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    int N = 5000000;
-    int* arr = NULL;
+    const int N =  1000000;
+    int arr[N];
 
     if (rank == 0) {
-        arr = (int*)malloc(N * sizeof(int));
-        if (!arr) {
-            fprintf(stderr, "Memory allocation failed\n");
-            MPI_Abort(MPI_COMM_WORLD, 1);
-        }
-
-        srand(time(NULL));
-        for (int i = 0; i < N; i++) {
-            arr[i] = rand() % 100 + 1;
-        }
+     for (int i = 0; i < N; i++) {
+       arr[i] = rand() % 100 + 1;
     }
-
-    if (rank != 0) {
-        arr = (int*)malloc(N * sizeof(int));
-        if (!arr) {
-            fprintf(stderr, "Memory allocation failed\n");
-            MPI_Abort(MPI_COMM_WORLD, 1);
-        }
+        srand(time(NULL));
     }
 
     double start_time = MPI_Wtime();
 
     MPI_Bcast(arr, N, MPI_INT, 0, MPI_COMM_WORLD);
 
-    int chunk_size = (N + size - 1) / size;
+    int chunk_size = N / size;
     int start = rank * chunk_size;
     int end = (start + chunk_size > N) ? N : start + chunk_size;
 
-    int local_sum = 0;
+    long long local_sum = 0;
     for (int i = start; i < end; i++) {
         local_sum += arr[i];
     }
 
-    int total_sum = 0;
-    MPI_Reduce(&local_sum, &total_sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    long long total_sum = 0;
+    MPI_Reduce(&local_sum, &total_sum, 1, MPI_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
 
     double end_time = MPI_Wtime();
 
     if (rank == 0) {
-        printf("Total sum: %d\n", total_sum);
+        printf("Total sum: %llu\n", total_sum);
         printf("Elapsed time: %.6f seconds\n", end_time - start_time);
     }
 
-    free(arr);
     MPI_Finalize();
     return 0;
 }
--- mpi-sum-c/wave_sum.c
+++ mpi-sum-c/wave_sum.c
@@ -3,8 +3,6 @@
 #include <stdlib.h>
 #include <time.h>
 
-typedef long long ll;
-
 int main(int argc, char **argv) {
     MPI_Init(&argc, &argv);
 
@@ -12,69 +10,61 @@ int main(int argc, char **argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world);
 
-    int N = 5000000; 
-    ll *array = NULL;
+    const int N = 900000;
+    long long array[N];
 
     if (rank == 0) {
-        array = (ll*)malloc(sizeof(ll) * N);
         srand((unsigned)time(NULL));
         for (int i = 0; i < N; i++)
             array[i] = rand() % 100;
-
-        //printf("Initial array: ");
-        //for (int i = 0; i < N; i++) printf("%lld ", array[i]);
-        //printf("\n");
     }
 
-    MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    if (rank != 0) array = (ll*)malloc(sizeof(ll) * N);
+    MPI_Bcast((void*)array, N, MPI_LONG_LONG, 0, MPI_COMM_WORLD);
 
     int len = N;
     int wave = 0;
 
     double start_time = MPI_Wtime();
 
-    while (len > 1) {
-        MPI_Bcast(array, len, MPI_LONG_LONG, 0, MPI_COMM_WORLD);
+    long long local_res[N];
+    long long root_res[N];
 
+    while (len > 1) {
         int pairs = len / 2;
-        ll *local_res = (ll*)calloc(pairs, sizeof(ll));
+
+        for (int i = 0; i < pairs; i++)
+            local_res[i] = 0;
 
         for (int k = rank; k < pairs; k += world) {
             int j = len - 1 - k;
             local_res[k] = array[k] + array[j];
         }
 
-        ll *root_res = NULL;
-        if (rank == 0) root_res = (ll*)calloc(pairs, sizeof(ll));
+        for (int i = 0; i < pairs; i++)
+            root_res[i] = 0;
 
         MPI_Reduce(local_res, root_res, pairs, MPI_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
 
         if (rank == 0) {
             wave++;
+
             for (int i = 0; i < pairs; i++)
                 array[i] = root_res[i];
 
             len = pairs + (len % 2);
-            // printf("Wave %d (len=%d): ", wave, len);
-            // for (int i = 0; i < len; i++) printf("%lld ", array[i]);
-            // printf("\n");
         }
 
-        free(local_res);
-        if (rank == 0) free(root_res);
-
         MPI_Bcast(&len, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Bcast(array, len, MPI_LONG_LONG, 0, MPI_COMM_WORLD);
     }
 
     double end_time = MPI_Wtime();
 
     if (rank == 0) {
-        // printf("Final sum: %lld\n", array[0]);
+        printf("Final sum: %lld\n", array[0]);
         printf("Total time: %.6f seconds\n", end_time - start_time);
     }
 
-    free(array);
     MPI_Finalize();
     return 0;
 }
